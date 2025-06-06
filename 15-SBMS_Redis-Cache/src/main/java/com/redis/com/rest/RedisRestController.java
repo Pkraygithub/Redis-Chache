273c5c0165ba31ae20c65270a9d59a84ceae8358
Pkraygithub <pankaj9576.kr@gmail.com>
2025-06-06 20:35:59 +0530

    Second Commit

--- 15-SBMS_Redis-Cache/src/main/java/com/redis/com/rest/RedisRestController.java
+++ 15-SBMS_Redis-Cache/src/main/java/com/redis/com/rest/RedisRestController.java
@@ -1,7 +1,10 @@
 package com.redis.com.rest;
 
+import java.util.List;
+
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,18 +25,29 @@ public class RedisRestController {
 	}
 	
 	
-	@PostMapping("/store")
+	@PostMapping("/user")
 	public String sotreData(@RequestBody User  usre) {
 		hasOps.put("Person", usre.getUid(),  usre);
 		System.out.println("Program Will Be Executed....🥰🥰🥰🥰");
 		return "Success";
 	}
 	
-	@GetMapping("/data/{uid}")
+	@GetMapping("/user/{uid}")
 	public User sotreData(@PathVariable Integer uid) {
 		User user =  hasOps.get("Person",uid);
 		return user;
 	}
 	
+	@GetMapping("/allusers")
+	public List<User>  getAllUser() {
+		return hasOps.values("Person");
+	}
+	
+	
+	@DeleteMapping("/userdelete/{uid}")
+	public String  deleteUser(@PathVariable Integer uid) {
+		hasOps.delete("Person", uid);
+		return "Deleted Successfully";
+	}
 	
 }
